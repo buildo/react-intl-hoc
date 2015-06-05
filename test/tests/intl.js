1d419bbae01cd7966f6f70e842aaf783dafdf9f8
Giovanni Gonzaga <giovanni@buildo.io>
2015-06-05 12:52:46 +0200

    basic @intl tests

--- test/tests/intl.js
+++ test/tests/intl.js
@@ -1,9 +1,30 @@
-const expect = require('expect');
+import expect from 'expect';
+import React from 'react';
+import {
+  contextTypes,
+  childContextTypes
+} from 'react-intl';
+import { intl } from '../../src';
+
+class Component extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+const IntlComponent = intl(Component);
 
 describe('intl decorator', () => {
 
-  it('should pass :P', () => {
+  it('should add intl context statics', () => {
+    expect(IntlComponent.contextTypes).toEqual(contextTypes);
+    expect(IntlComponent.childContextTypes).toEqual(childContextTypes);
+  });
 
+  it('should render the decorated Component', () => {
+    const rendered = (new IntlComponent()).render();
+
+    expect(rendered.type.name).toBe('Component');
   });
 
 });
