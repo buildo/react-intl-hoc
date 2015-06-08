ba2052000bb26667f57f9b7f1001fbdbf9cc68dd
Giovanni Gonzaga <giovanni@buildo.io>
2015-06-08 18:18:20 +0200

    load polyfill if missing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-if (window.Intl) {
-  import intl from './intl';
-  import IntlComponentWrapper from './IntlComponentWrapper';
-  import FormattedMessage from './FormattedMessage';
-  import FormattedDate from './FormattedDate';
-  import FormattedNumber from './FormattedNumber';
-  import FormattedRelative from './FormattedRelative';
-  import FormattedTime from './FormattedTime';
-
-  export default {
-    intl,
-    IntlComponentWrapper,
-    FormattedMessage,
-    FormattedDate,
-    FormattedNumber,
-    FormattedRelative,
-    FormattedTime
-  };
-} else {
-  throw new Error('no intl found');
+if (!window.Intl) {
+  require('intl');
 }
+
+import intl from './intl';
+import IntlComponentWrapper from './IntlComponentWrapper';
+import FormattedMessage from './FormattedMessage';
+import FormattedDate from './FormattedDate';
+import FormattedNumber from './FormattedNumber';
+import FormattedRelative from './FormattedRelative';
+import FormattedTime from './FormattedTime';
+
+export default {
+  intl,
+  IntlComponentWrapper,
+  FormattedMessage,
+  FormattedDate,
+  FormattedNumber,
+  FormattedRelative,
+  FormattedTime
+};
